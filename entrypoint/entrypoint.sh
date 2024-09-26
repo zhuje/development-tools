48bc5e477545310621a535cc705ffe350af9267c
Jenny Zhu <jenny.a.zhu@gmail.com>
2024-09-25 23:36:28 -0400

    Update entrypoint.sh

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -35,57 +35,48 @@ deploy_dashboards() {
 }
 
 deploy_korrel8r(){
-    (cd  ../coo && oc apply -f ui-plugin-logging.yaml && oc apply -f ui-plugin-troubleshooting.yaml )
+    printf "\n${GREEN} *** Deploying Korrel8r, Logging, Network Observability *** ${ENDCOLOR}\n"
+    (cd  ../coo && oc apply -f ui-plugin-logging.yaml )
     (cd  ../korrel8r && cd openshift && make operators && make resources)
 }
 
-printf "\n${GREEN} OCP version: $OCP_VERSION ${ENDCOLOR}\n"
+deploy_troubleshooting(){
+    printf "\n${GREEN} *** Deploying Troubleshooting *** ${ENDCOLOR}\n"
+    (cd  ../coo && oc apply -f ui-plugin-troubleshooting.yaml )
+}
 
+printf "\n${GREEN} OCP version: $OCP_VERSION ${ENDCOLOR}\n"
 
 # Deploy UIPlugins and Observability Operator based on OpenShift Version 
 if [[ $(bc <<< "$OCP_VERSION == 4.11") -eq 1 ]]; then
     echo "OCP 4.11 compatabile resources..."
     echo "COO: 0.2.0"
     echo "UIPlugins: dashboards\n"
-    # (cd  ../coo && oc apply -f ui-plugin-dashboards.yaml )
-    # ./../dashboards/deploy-dashboards.sh
+
+    deploy_observability_operator $OPERATOR_BUNDLE_V02
+    deploy_dashboards
+    oc patch consoles.operator.openshift.io/cluster --type=merge --patch '{ "spec": { "plugins": ["monitoring-plugin", "console-dashboards-plugin"]}}'
 
 elif [[ $(bc <<< "$OCP_VERSION >= 4.12") -eq 1 ]] && [[ $(bc <<< "$OCP_VERSION <= 4.15") -eq 1 ]]; then
     echo "OCP 4.12 - 4.15 compatible resources..."
     echo "COO: 0.3.0+"
     echo "UIPlugins: dashboards, distributed tracing, logging\n"
-    # ./../dashboards/deploy-dashboards.sh
+
+    deploy_observability_operator $OPERATOR_BUNDLE_V03
+    deploy_dashboards
+    deploy_korrel8r # UIPlugin and Resources for korrel8r, logging, troubleshooting, net observe  
+    oc patch consoles.operator.openshift.io/cluster --type=merge --patch '{ "spec": { "plugins": ["monitoring-plugin", "netobserv-plugin", "logging-view-plugin", "console-dashboards-plugin"]}}'
 
 elif [[ $(bc <<< "$OCP_VERSION >= 4.16") -eq 1 ]]; then
     echo "OCP 4.16+ compatible resources..."
     echo "COO: 0.3.0+"
-    echo "UIPlugins: dashboards, distributed tracing, logging, troubleshooting"
+    echo "UIPlugins: dashboards, distributed tracing, logging, troubleshooting\n"
 
-   # deploy_observability_operator $OPERATOR_BUNDLE_V04
-
-    # dashboards
-    # (cd  ../coo && oc apply -f ui-plugin-dashboards.yaml )
-    # (cd  ../dashboards && ./deploy-dashboards.sh )
-
-    # deploy_dashboards
-
-    # # logging, troubleshooting, net observe  
-    # # (cd  ../coo && oc apply -f ui-plugin-logging.yaml && oc apply -f ui-plugin-troubleshooting.yaml )
-    # # (cd  ../korrel8r && cd openshift && make operators && make resources)
-
-    deploy_korrel8r
-
+    deploy_observability_operator $OPERATOR_BUNDLE_V04
+    deploy_dashboards
+    deploy_korrel8r   # UIPlugin and Resources for korrel8r, logging, net observe  
+    deploy_troubleshooting
+    oc patch consoles.operator.openshift.io/cluster --type=merge --patch '{ "spec": { "plugins": ["monitoring-plugin", "netobserv-plugin", "logging-view-plugin", "console-dashboards-plugin", "troubleshooting-panel-console-plugin"]}}'
 else
     echo "OCP version not supported"
 fi
-
-
-
-
-# # operator-sdk run bundle \
-# #     quay.io/gbernal/observability-operator-bundle:0.4.0-dev-tp \
-# #     --install-mode AllNamespaces \
-# #     --namespace openshift-operators \
-# # 	--security-context-config restricted
-
-

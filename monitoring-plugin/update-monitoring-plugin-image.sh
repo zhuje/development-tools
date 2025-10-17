f64f9d9a9ee346e8e9c8c3142026efadbbbb265c
Evelyn Tanigawa Murasaki <emurasak@redhat.com>
2025-10-17 15:32:45 -0300

    monitoring-plugin image injection

---
+++ monitoring-plugin/update-monitoring-plugin-image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+read -p "MP_IMAGE: " mp_image
+export MP_IMAGE="$mp_image"
+
+echo "--------------------------------"
+echo "MP_IMAGE: ${MP_IMAGE}"
+echo "--------------------------------"
+
+CMO_FILE="/tmp/cmo_monitoring_csv_$(date +%s%N).yaml"
+
+oc get deployment cluster-monitoring-operator -n openshift-monitoring -o yaml > "${CMO_FILE}" --kubeconfig "${KUBECONFIG}"
+
+sed -i "s#value: .*monitoring-plugin.*#value: ${MP_IMAGE}#g" "${CMO_FILE}"
+sed -i "s#^\([[:space:]]*- -images=monitoring-plugin=\).*#\1${MP_IMAGE}#g" "${CMO_FILE}"
+
+oc patch clusterversion version --type json -p "$(cat disable-monitoring.yaml)"
+
+oc replace -f "${CMO_FILE}" --kubeconfig "${KUBECONFIG}"
+
+sleep 30
+
+echo "--------------------------------"
+echo "Cluster monitoring operator"
+echo "--------------------------------"
+csv=$(oc get deployment cluster-monitoring-operator -n openshift-monitoring -o yaml)
+echo "${csv}"
+echo "--------------------------------"
+echo "Monitoring plugin"
+echo "--------------------------------"
+csv=$(oc get deployment monitoring-plugin -n openshift-monitoring -o yaml)
+echo "${csv}"
+echo "--------------------------------"
+
+rm -f "${CMO_FILE}"
